f49bc5fceaf92d1bcdf896f0b439a0fffd5696d5
di4m0nds <ws.2000.sja@gmail.com>
2024-06-18 16:13:24 +0200

    Initial project setup and implementation of authentication views
    - Added .gitignore file to exclude node_modules, .env, and other unnecessary files
    - Created config.js for application configuration settings
    - Created db/User.json to store user data
    - Implemented server setup in index.js
    - Added package.json for managing project dependencies
    - Added pnpm-lock.yaml for pnpm dependency management
    - Implemented user repository functions in user-repository.js
    - Created views:
      - auth.ejs: Login and registration form with Apple-inspired design
      - example.ejs: Example template
      - protected.ejs: Protected content template

---
+++ index.js
@@ -0,0 +1,116 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import { TEACHER_LINK, TEACHER_NAME, PORT, SECRET_TOKEN_KEY, ACCESS_TOKEN } from './config.js'
+
+import { UserRepository } from './user-repository.js'
+import cookieParser from 'cookie-parser'
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(express.json()) // Check if the request has a body (for "req.body")
+app.use(cookieParser()) // Allow us to use & handle cookies
+
+app.use((req, res, next) => {
+  const token = req.cookies[ACCESS_TOKEN]
+
+  req.session = { user: null }
+
+  try {
+    const data = jwt.verify(token, SECRET_TOKEN_KEY)
+    req.session.user = data
+  } catch (_) {}
+
+  next()
+})
+
+app.get('/welcome', (_, res) => res.render('example', { link: TEACHER_LINK, author: TEACHER_NAME }))
+app.get('/', (req, res) => {
+  // We can remove this cause we have a new middleware that handle this
+  // const token = req.cookies[ACCESS_TOKEN]
+  // if (!token) {
+  //   return res.status(403).render('auth')
+  // }
+  // try {
+  //   const data = jwt.verify(token, SECRET_TOKEN_KEY)
+  //   res.render('auth', data)
+  // } catch (error) {
+  //   res.status(401).render('protected')
+  // }
+
+  const { user } = req.session
+  res.render('auth', user)
+})
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const user = await UserRepository.login({ username, password })
+    const token = jwt.sign(
+      { id: user.id, username: user.username },
+      SECRET_TOKEN_KEY,
+      { expiresIn: '1h' }
+    )
+
+    res
+      .status(200)
+      .cookie(
+        ACCESS_TOKEN,
+        token,
+        {
+          httpOnly: true,
+          secure: false, // process.env.NODE_ENV === 'PROD',
+          sameSite: 'strict',
+          maxAge: 1000 * 60 * 60
+        }
+      )
+      .send({
+        user,
+        message: `User ${username} has been logged in.`
+      })
+  } catch (error) {
+    // It isn't a good idea sending the repository error to the client, better handling it.
+    res.status(400).send({ error: error.message })
+  }
+})
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const id = await UserRepository.create({ username, password })
+
+    res.status(201).json({
+      username,
+      id,
+      message: `User ${username} has been created.`
+    })
+  } catch (error) {
+    // It isn't a good idea sending the repository error to the client, better handling it.
+    res.status(400).send({ error: error.message })
+  }
+})
+app.post('/logout', (req, res) => {
+  res.clearCookie(ACCESS_TOKEN).json({ message: 'Logout successfully.' })
+})
+
+app.get('/protected', (req, res) => {
+  // const token = req.cookies[ACCESS_TOKEN]
+  // if (!token) {
+  //   return res.status(403).render('protected')
+  // }
+
+  // try {
+  //   const data = jwt.verify(token, SECRET_TOKEN_KEY)
+  //   res.render('protected', data)
+  // } catch (error) {
+  //   res.status(401).render('protected')
+  // }
+  const { user } = req.session
+  if (!user) return res.status(403).send('Access not authorized.')
+  res.render('protected', user)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}, http://localhost:${PORT}/`)
+})
